4efc4e78346b3a311fb05674afd8622671669be5
furaha707 <zxcvyein950823@gmail.com>
2022-05-30 10:11:32 +0900

    yeonkyo

---
+++ include/arr_data.php
@@ -0,0 +1,69 @@
+<?
+
+$site_title = "(주)연교"; // 사이트명
+
+$P_lang = "ko"; // html language
+
+$P_company_name = "(주)연교"; // 회사명
+
+$P_addr1 = "경기도 용인시 기흥주 중부대로 184, 23층(힉스유타워)"; // 주소1
+$P_addr2 = ""; // 주소2
+
+$P_tel1 = "031-546-0835";
+$P_tel2 = "";
+
+$P_fax1 = "031-546-0835";
+$P_fax2 = "";
+
+$P_email1 = "info@yeonkyo.co.kr";
+$P_email2 = "";
+
+$P_business_no = "381-86-01940"; // 사업자등록번호
+
+$P_sales_no = ""; // 통신판매업신고번호
+
+// 네비게이션 메뉴명
+$nav_1 = "Home";
+$nav_2 = "Company";
+$nav_3 = "Leadership";
+$nav_4 = "Our Team";
+$nav_5 = "Contact";
+
+$nav_policy = "홈페이지 정책";
+$nav_policy_1 = "개인정보처리방침";
+$nav_policy_2 = "이용약관";
+$nav_policy_3 = "이메일무단수집거부";
+
+// 네비게이션 하이퍼링크
+$nav_1_href = "../main/main.html";
+$nav_2_href = "../sub/company.html";
+$nav_3_href = "../sub/leadership.html";
+$nav_4_href = "../sub/ourteam.html";
+$nav_5_href = "../sub/contact.html";
+/*
+$nav_1_1_href = "../template/template.html";
+$nav_1_2_href = "../template/board-list.html";
+$nav_1_3_href = "../template/calendar.html";
+$nav_1_4_href = "../template/location.html";
+$nav_1_5_href = "../template/find_account.html";
+$nav_1_6_href = "../template/sns-login.html";
+$nav_1_7_href = "../template/hover-effect.html";
+*/
+// $nav_1_2_href = "../test/dp3-test__alpha.html";
+// $nav_1_2_1_href = "../test/dp3-test__alpha.html";
+// $nav_1_2_2_href = "../test/dp3-test__bravo.html";
+// $nav_1_2_3_href = "../test/dp3-test__charlie.html";
+
+$nav_policy_href = "../policy/privacy.html";
+$nav_policy_1_href = "../policy/privacy.html";
+$nav_policy_2_href = "../policy/term.html";
+$nav_policy_3_href = "../policy/email-security.html";
+
+// 서브비주얼
+$sv_1_name = "";
+$sv_2_name = "";
+$sv_3_name = "";
+$sv_4_name = "";
+$sv_5_name = "";
+
+?>
